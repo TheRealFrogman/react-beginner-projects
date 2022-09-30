a931fc70fc01f9f21be56b4549e259a723dca9ea
frogman <krohinmisha99@gmail.com>
2022-09-30 21:01:31 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './index.scss';
+import "./index.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+   return (
+      <div className="App">
+         <div>
+            <h2>Счетчик:</h2>
+            <h1>123123</h1>
+            <button className="minus">- Минус</button>
+            <button className="plus">Плюс +</button>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
