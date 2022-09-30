e9a1603a0b844af36be724ffcdbbd4b2bd8cae10
frogman <krohinmisha99@gmail.com>
2022-09-30 16:52:14 +0500

    господи куда жать

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+   const [userList, setUserList] = useState(null);
+   const [isLoading, setLoading] = useState(true);
+
+   // async function getUsers() {
+   //    await fetch("https://reqres.in/api/users")
+   //       .then((response) => response.json())
+   //       .then((result) => {
+   //          setUserList(result.data);
+
+   //          setLoading(false);
+   //       })
+   //       .catch((err) => console.warn(err));
+   // }
+   useEffect(() => {
+      fetch("https://reqres.in/api/users")
+         .then((response) => response.json())
+         .then((result) => {
+            setUserList(result.data);
+         })
+         .catch((err) => console.warn(err))
+         .finally(() => setLoading(false));
+   }, []);
+
+   return (
+      <div className="App">
+         <Users isLoading={isLoading} userList={userList} />
+         {/* <Success /> */}
+      </div>
+   );
 }
 
 export default App;
