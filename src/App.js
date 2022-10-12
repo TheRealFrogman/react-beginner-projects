9554caf7dfbe276923bac73cf94e42ec262d100e
frogman <krohinmisha99@gmail.com>
2022-10-13 00:35:13 +0500

    i completed the password generator

--- src/App.js
+++ src/App.js
@@ -1,43 +1,144 @@
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+
+const MINIMAL_LENGTH = 8;
+const MAXIMAL_LENGTH = 18;
+const PASSWORD_CHARS = {
+    numbers: "1234567890",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    symbols: "~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/",
+};
+const DEFAULT_CHECKBOXES = {
+    lowerCase: true,
+    upperCase: false,
+    symbols: false,
+    numbers: true,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <span className="subtitle">Пароль:</span>
-      <h2>2UYsAhMdPR7wBHwG</h2>
-      <div className="row">
-        <span>Длина паролья:</span>
-        <input className="pwg-length" type="text" value="10" />
-      </div>
-      <div className="row">
-        <span>Прописные буквы:</span>
-        <input class="styled-checkbox" id="checkbox1" type="checkbox" value="value1" />
-        <label for="checkbox1" />
-      </div>
-      <div className="row">
-        <span>Строчные буквы:</span>
-        <input
-          checked={true}
-          class="styled-checkbox"
-          id="checkbox2"
-          type="checkbox"
-          value="value1"
-        />
-        <label for="checkbox2" />
-      </div>
-      <div className="row">
-        <span>Цифры:</span>
-        <input class="styled-checkbox" id="checkbox3" type="checkbox" value="value1" />
-        <label for="checkbox3" />
-      </div>
-      <div className="row">
-        <span>Символы:</span>
-        <input class="styled-checkbox" id="checkbox4" type="checkbox" value="value1" />
-        <label for="checkbox4" />
-      </div>
-      <button>Сгенерировать</button>
-    </div>
-  );
+    const [password, setPassword] = useState("");
+    const [passLength, setPassLength] = useState(MINIMAL_LENGTH);
+    const [checkBoxes, setCheckboxes] = useState(DEFAULT_CHECKBOXES);
+    const [stateCopied, setCopied] = useState(false);
+
+    const onClickPassword = (event, password) => {
+        navigator.clipboard.writeText(password);
+        setCopied(true);
+    };
+    const onChangeLength = (value) => {
+        if (value >= MINIMAL_LENGTH) setPassLength(value);
+    };
+    const onChangeCheckBox = (event) => {
+        const target = event.target.id;
+        setCheckboxes((prev) => {
+            return { ...prev, [target]: !prev[target] };
+        });
+        // console.log(checkBoxes);
+    };
+
+    const generatePassowrd = (length, options, chars) => {
+        let computedCharset = "";
+        for (let key in options) {
+            if (options[key]) computedCharset += chars[key];
+        }
+        let result = "";
+
+        for (let i = 0; i < length; i++) {
+            result +=
+                computedCharset[
+                    Math.floor(Math.random() * computedCharset.length)
+                ];
+        }
+        setPassword(result);
+        // console.log(computedCharset);
+    };
+    useEffect(() => {
+        generatePassowrd(passLength, checkBoxes, PASSWORD_CHARS);
+    }, []);
+    return (
+        <div className="App noselect">
+            <span className="subtitle">Password:</span>
+            <h2
+                className={`password noselect ${stateCopied ? "copied" : ""}`}
+                onClick={(event) => onClickPassword(event, password)}
+                onAnimationEnd={() => setCopied(false)}
+            >
+                {password}
+            </h2>
+            <div className="row">
+                <span>Pass length:</span>
+                <span className="pass-length">
+                    {passLength}
+                    <input
+                        type="range"
+                        step={1}
+                        min={MINIMAL_LENGTH}
+                        max={MAXIMAL_LENGTH}
+                        onChange={(event) => onChangeLength(event.target.value)}
+                        value={passLength}
+                    />
+                </span>
+            </div>
+            <div className="row">
+                <span>Uppercase chars:</span>
+                <input
+                    disabled={checkBoxes.lowerCase ? false : true}
+                    className={`styled-checkbox ${
+                        checkBoxes.lowerCase ? "" : "disabled"
+                    }`}
+                    onChange={onChangeCheckBox}
+                    id="upperCase"
+                    type="checkbox"
+                    checked={checkBoxes.upperCase}
+                />
+                <label htmlFor="upperCase" />
+            </div>
+            <div className="row">
+                <span>Lowercase chars:</span>
+                <input
+                    disabled={checkBoxes.upperCase ? false : true}
+                    className={`styled-checkbox ${
+                        checkBoxes.upperCase ? "" : "disabled"
+                    }`}
+                    onChange={onChangeCheckBox}
+                    id="lowerCase"
+                    type="checkbox"
+                    checked={checkBoxes.lowerCase}
+                />
+                <label htmlFor="lowerCase" />
+            </div>
+            <div className="row">
+                <span>Numbers:</span>
+                <input
+                    checked={checkBoxes.numbers}
+                    onChange={onChangeCheckBox}
+                    className="styled-checkbox"
+                    id="numbers"
+                    type="checkbox"
+                />
+                <label htmlFor="numbers" />
+            </div>
+            <div className="row">
+                <span>Symbols:</span>
+                <input
+                    checked={checkBoxes.symbols}
+                    onChange={onChangeCheckBox}
+                    className="styled-checkbox"
+                    id="symbols"
+                    type="checkbox"
+                />
+                <label htmlFor="symbols" />
+            </div>
+            <button
+                onClick={() =>
+                    generatePassowrd(passLength, checkBoxes, PASSWORD_CHARS)
+                }
+            >
+                Generate
+            </button>
+        </div>
+    );
 }
 
 export default App;
